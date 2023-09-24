322b162e83b7d0cd2e49a2ba4c1298b9cb6400c3
mohit792 <110386553+mohit792@users.noreply.github.com>
2023-09-24 23:43:29 +0530

    db pass

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("chattify", "user", "Jesus@1234", {
+const sequelize = new Sequelize("chattify", "user", process.env.DB_PASSWORD, {
       host: process.env.MYSQL_HOST,
       // host: "16.170.227.222",
       dialect: "mysql" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
